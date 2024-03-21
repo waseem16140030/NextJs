96276b8271bbc4272809ad01843e310df304c6f9
Waseem <waseem@viwell.com>
2024-03-21 12:19:40 +0500

    feat: completed chapter-5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Customer({}: Props) {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface DashboardProps {}
+
+const Dashboard: React.FunctionComponent<DashboardProps> = (props) => {
+  return <p>Dashboard Page</p>;
+};
+
+export default Dashboard;
